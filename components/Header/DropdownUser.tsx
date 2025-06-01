d6411f2dcf7b1b7f33e687ea32d9e129908dc5ab
Mohamed Farjallah <206404158+ii8i8j@users.noreply.github.com>
2025-06-01 22:56:30 +0300

    .

--- components/Header/DropdownUser.tsx
+++ components/Header/DropdownUser.tsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import ClickOutside from "@/components/ClickOutside";
+import { useRouter } from "next/navigation";
 
 const DropdownUser = () => {
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
+  const route = useRouter()
   return (
     <ClickOutside onClick={() => setDropdownOpen(false)} className="relative">
       <Link
@@ -128,7 +130,7 @@ const DropdownUser = () => {
               </Link>
             </li>
           </ul>
-          <button className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
+          <button onClick={() => route.push("/login")} className="flex items-center gap-3.5 px-6 py-4 text-sm font-medium duration-300 ease-in-out hover:text-primary lg:text-base">
             <svg
               className="fill-current"
               width="22"
