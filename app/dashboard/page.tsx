a023a556f277c1bb23f49c8a55dd42d3e3d89619
Ibrahim_Yaseen <abrahimyaseen2811@gmail.com>
2025-06-01 16:27:22 +0300

    first commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "@/components/dashboardHelper/DefaultLayout";
+import LayoutDash from "@/components/dashboardHelper/LayoutDash";
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <LayoutDash />
+      </DefaultLayout>
+    </>
+  );
+}
